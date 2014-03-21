c0501ece40a56c74502fd6b3b3b7b208143a4ff9
Nicola Sturaro Sommacal <nicola.sturaro@quantide.com>
2014-03-21 23:51:33 +0100

    release 0.1.2

--- R/mapIT.R
+++ R/mapIT.R
@@ -1,8 +1,8 @@
 ############################################################
 ### mapIT() function
 # Authors: Davide Massidda and Nicola Sturaro
-# Date: Mar 4, 2014
-# Version: 0.1
+# Date: Mar 14, 2014
+# Version: 0.1.2
 # Requires: ggplot2
 # License: GPLv3
 
@@ -10,9 +10,12 @@
 ############################################################
 
 mapIT <- function(
-  values, id = NULL, data = NULL, detail = "regions", dataSource = "istat", guide.label = deparse(substitute(values)),
+  values, id = NULL, data = NULL, detail = "regions", dataSource = "istat", sub = NULL,
+  discrete = ifelse(is.numeric(values), FALSE, TRUE),
+  guide.label = deparse(substitute(values)),
   graphPar = list(
-    low = "#f0f0f0", high = "#005096", title = NULL,
+    low = "#f0f0f0", high = "#005096", palette = "BuGn", colours = NULL,
+    title = NULL,
     theme = theme_minimal(),
     themeOption = list(
       title = element_text(size = 18), axis.ticks = element_blank(),
@@ -22,8 +25,14 @@ mapIT <- function(
     legendTitle = guide.label
   ) 
 ) {
-    
+      
   ### Check inputs
+  if(is.null(id)) {
+    warning("id not provided. values assigned by order")
+  }  
+  if(detail != "regions") {
+    warning("detail is currently ignored")
+  }
   if(class(dataSource) == "character") {
     if (dataSource != "istat") {
       stop("dataSource must be 'istat' or a shape data")
@@ -36,18 +45,36 @@ mapIT <- function(
     if(detail != "regions") {
       warning("shapefile provided. detail is currently ignored")
     }
-  }
-  if(detail != "regions") {
+  }  
+  if(! is.null(sub)) {
     warning("detail is currently ignored")
   }
-  if(is.null(id)) {
-    warning("id not provided. values assigned by order")
+  if(! all(sort(names(graphPar)) %in% sort(names(eval(formals(sys.function(sys.parent()))$graphPar))))) {
+    warning("additional arguments to graphPar ignored")
   }
+  if(!is.null(graphPar$themeOption)) {
+    if(! all(sort(names(graphPar$themeOption)) %in% sort(names(eval(formals(sys.function(sys.parent()))$graphPar$themeOption))))) {
+      warning("additional arguments to themeOption ignored")
+    }
+  }
+  
   
   ### Remove all non alphanumeric characters from region names and transform to lower case
   onlyChar <- function(string) {
     tolower(gsub(" ", "", gsub("[^[:alnum:]]", " ", string)))
   }
+
+  
+  ### Update lists elements (and keep default when argument is not passed)
+  listDefTO = eval(formals(sys.function(sys.parent()))$graphPar$themeOption)
+  if(!is.null(graphPar$themeOption)) {listDefTO[sort(names(listDefTO[sort(names(listDefTO)) %in% sort(names(graphPar$themeOption))]))] = graphPar$themeOption[sort(names(graphPar$themeOption))]}
+    
+  listDef = eval(formals(sys.function(sys.parent()))$graphPar)
+  listDef[sort(names(listDef[names(listDef) %in% names(graphPar)]))] = graphPar[sort(names(graphPar))]
+  
+  graphPar = listDef
+  graphPar$themeOption = listDefTO
+
   
   ### If data exists then search values and id as data columns
   if(!is.null(data)) {
@@ -55,15 +82,30 @@ mapIT <- function(
     if(!is.null(id)) {id = data[, id]}
   }
   
+  ### Transform values to factor
+  if(discrete == TRUE) {values = as.factor(values)}
+  
+  
   ### If id is null then assign numbers from 0
   if(is.null(id)) {id = 0:(length(values)-1)}
+  
   
   ### If guide.label contains $, keep the second part
   if(grepl("\\$", guide.label))
     guide.label <- unlist(strsplit(guide.label, "\\$"))[2]
   
+  
+  ### If guide.label contain non alphanumeric characters, remove them
+  guide.label <- onlyChar(guide.label)
+    
+  
   ### If dataSource is a dataframe use it
   if(class(dataSource) == "data.frame") {shapedata = dataSource}
+  
+  
+  ### If dataSource is a string load data
+  if(class(dataSource) == "character" & dataSource == "istat" & detail == "regions") {data(shapefile_istat_regioni); shapedata = shapedata_istat_regioni}
+  
   
   ### Match region ID or names
   if(is.numeric(id)) {
@@ -73,28 +115,61 @@ mapIT <- function(
     pos <- match(onlyChar(shapedata$region), onlyChar(id))
     ck <- match(onlyChar(id), onlyChar(shapedata$region))
   }
+  
+  
+  ### Select 'sub' regions
+  ### TODO
+#   if (!is.null(sub)) {
+#     if(is.numeric(id)) {
+#       posSub <- match(shapedata$id, as.character(sub))
+#       ckSub <- match(as.character(sub), shapedata$id)
+#       shapedata <- shapedata[shapedata$id %in% as.character(sub), ]
+#       values <- values[id %in% as.character(sub)]
+#     } else { 
+#       posSub <- match(onlyChar(shapedata$region), onlyChar(sub))
+#       ckSub <- match(onlyChar(sub), onlyChar(shapedata$region))
+#       shapedata <- shapedata[onlyChar(shapedata$region) %in% onlyChar(sub), ]
+#       values <- values[id %in% onlyChar(sub)]
+#     }  
+#   }
+  
+#  if(sum(is.na(posSub)) > 0) {
+#    warning(paste("Some 'sub' not recognized:", paste(id[is.na(ckSub)], collapse = ", ")))
+#  }
+
 
   ### Check if some region ID or name is not matched
   if(sum(is.na(pos)) > 0) {
     warning(paste("Some ID not recognized:", paste(id[is.na(ck)], collapse = ", ")))
   }
+
   
   ### Add values to shape data
   shapedata[, guide.label] <- values[pos]
   
-  ### Remove unused variables
-  rm(values, id, pos)
-  
+
   ### Plot building
   gp <- ggplot(shapedata, aes_string(x = "long", y = "lat"))
   bg <- graphPar$theme
   th <- do.call(theme, graphPar$themeOption)
-  col <- scale_fill_continuous(aes_string(guide.label), low = graphPar$low, high = graphPar$high)
-  #col <- scale_fill_discrete(breaks=as.character(1:20))
+  sfc <- scale_fill_continuous(graphPar$legendTitle, low = graphPar$low, high = graphPar$high)
+  sfb <- scale_fill_brewer(graphPar$legendTitle, labels=levels(as.factor(values)), palette = graphPar$palette)
+  sfm <- scale_fill_manual(graphPar$legendTitle, values = graphPar$colours)
   map <- geom_map(aes_string(map_id = "region", fill = guide.label), map = shapedata, col = graphPar$borderCol, show_guide = graphPar$show_guide)
-  lab <- labs(x = "", y = "", title=graphPar$title, colour=guide.label)
-  leg <- guides(fill = guide_legend(title = graphPar$legendTitle))
-  out <- gp + bg + col + map + th + lab # + leg
+  lab <- labs(x = "", y = "", title=graphPar$title)
+  
+  out <- gp + bg + th + map + lab
+  
+  if(discrete == TRUE) {
+    if(is.null(graphPar$colours)) {
+      out <- out + sfb
+    } else {
+      out <- out + sfm 
+    }
+  } else {
+    out <- out + sfc
+  }
+  
   return(out)
 }
 
--- test.R
+++ test.R
@@ -1,23 +1,37 @@
 library(ggplot2)
 load("data/shapefile_istat_regioni.rda")
+load("~/Desktop/regionsIT.rda")
+shapedata_istat_regioni = regionsIT
+
+shapedata_istat_regioni$id = as.numeric(shapedata_istat_regioni$id)
+
+str(shapedata_istat_regioni)
+save(shapedata_istat_regioni, file="data/shapefile_istat_regioni.rda")
 
 
+values = c(1:20)
 values = runif(20,min=0,max=1000)          # crea dati sintetici
-values = c(rep(1,10), rep(2,10))
 id = 0:19                                  # crea i codici regionali
 
 id = c('Piemonte', 'VALLE D\'AOSTA', 'lOMBARDIA', 'TRENTINO-ALTO ADIGE', 'VENETO', 'FRIULI-VENEZIA GIULIA', 'LIGURIA', 'EMILIA-ROMAGNA', 'TOSCANA', 'UMBRIA', 'MARCHE', 'LAZIO', 'ABRUZZO', 'MOLISE', 'CAMPANIA', 'PUGLIA', 'BASILICATA', 'CALABRIA', 'SICILIA', 'SARDEGNA')
 
 df = data.frame(values3=values, id3=id)
 
-source('~/Dropbox/quantide/dev/mapIT/R/mapIT.R')
-mapIT(values=values, id=id, guide.label="Valori") # crea la mappa
+source('~/Dropbox/git/dev/mapIT/R/mapIT.R')
+mapIT(values=values, id=id, guide.label="Aree geografiche")
+mapIT(values=values, id=id, guide.label="Aree geografiche", graphPar = list(colours = c("red", "darkblue", "green", "yellow", "purple")))
 
+values = c(rep("Nord-Ovest", 4), rep("Nord-Est", 4), rep("Centro", 4), rep("Sud", 6), rep("Isole", 2))
+id = c('Piemonte', 'VALLE DAOSTA', 'lOMBARDIA', 'LIGURIA', 'Trentino Alto Adige', 'VENETO', 'FRIULI - VENEZIA GIULIA', 'EMILIAROMAGNA', 'TOSCANA', 'UMBRIA', 'MARCHE', 'LAZIO', 'ABRUZZO', 'MOLISE', 'CAMPANIA', 'PUGLIA', 'BASILICATA', 'CALABRIA', 'SICILIA', 'SARDEGNA')
+values = factor(values, levels = c("Nord-Ovest", "Nord-Est", "Centro", "Sud", "Isole"), ordered=TRUE)
+mapIT(values=values, id=id, guide.label="Areegeografiche", graphPar = list(title = "title", palette = "Dark2"))
+mapIT(values=runif(20,min=0,max=1000), id=id, guide.label="Valori", graphPar = list(low = "#000000", high = "#ff0000"))
+mapIT(values=runif(20,min=0,max=1000), guide.label="Valori") # crea la mappa
 
 
 shape=shapedata
-shape=shape[shape$region == "VENETO", ]
-mapIT(values=values, id=id, dataSource=shape, guide.label="Valori") # crea la mappa
+shape=shape[shape$region == "PIEMONTE", ]
+mapIT(values=45, id="PIEMONTE", dataSource=shape, guide.label="Valori") # crea la mappa
 names(shape)
 
 
